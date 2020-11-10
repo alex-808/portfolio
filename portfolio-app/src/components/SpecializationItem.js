c5dd9362c69437c765087af729207470f85b5c5e
Alex Davis <acdavis050@gmail.com>
2020-11-09 21:06:50 -0800

    Improved Specialization icon placement and background

--- portfolio-app/src/components/SpecializationItem.js
+++ portfolio-app/src/components/SpecializationItem.js
@@ -1,6 +1,14 @@
 function SpecializationItem() {
     return (
-		<div className="specialization_item">
+		<div className="spec_item">
+      <div className="icon_div">
+        <div className="icon_bg">
+          <img className="icon" src="fingerprint.svg" alt="white and black waves" width="100"></img>
+        </div>
+      </div>
+      <div className="spec_main_text">
+        <p>Item</p>
+      </div>
     </div>
     )
 }
