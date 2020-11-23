f2c23ea61b7ed068da8c5805209ef7a5da67ed04
Alex Davis <acdavis050@gmail.com>
2020-11-23 11:57:55 -0800

    Fixed 'isInstancedMesh bug' in Animation

--- portfolio-app/src/components/Footer/Footer.js
+++ portfolio-app/src/components/Footer/Footer.js
@@ -4,14 +4,14 @@ import './index.css';
 export class AboutMe extends Component {
     render() {
         return (
-            <section class="footer_section">
-                <div class="footer_accent">Call to Action</div>
-                <div class="social_icons">
-                    <img class="social_icon" src="imgs/checkmark.svg" />
-                    <img class="social_icon" src="imgs/checkmark.svg" />
-                    <img class="social_icon" src="imgs/checkmark.svg" />
+            <section className="footer_section">
+                <div className="footer_accent">Call to Action</div>
+                <div className="social_icons">
+                    <img className="social_icon" src="imgs/checkmark.svg" />
+                    <img className="social_icon" src="imgs/checkmark.svg" />
+                    <img className="social_icon" src="imgs/checkmark.svg" />
                 </div>
-                <div id="footer_animation"></div>
+                <div class="footer_animation"></div>
             </section>
         );
     }
