9c5889c37690b327a3d0f665a0407544250c4e61
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-18 16:51:25 -0700

    Add animate on scroll to specializations

--- portfolio-app/src/components/Specializations/SpecializationItems/SpecializationItem/SpecializationItem.js
+++ portfolio-app/src/components/Specializations/SpecializationItems/SpecializationItem/SpecializationItem.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './index.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import './index.css';
 
 export class SpecializationItem extends Component {
-    render() {
-        return (
-            <div className="spec_item">
-                <img className="spec_icon" src={this.props.item.icon_url} />
-                <h2 className="spec_title">{this.props.item.title}</h2>
-                {this.props.item.text}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    Aos.init({ offset: 0, duration: 700, easing: 'ease-in-out' });
+  }
+  render() {
+    return (
+      <div data-aos="flip-left" className="spec_item">
+        <img className="spec_icon" src={this.props.item.icon_url} />
+        <h2 className="spec_title">{this.props.item.title}</h2>
+        {this.props.item.text}
+      </div>
+    );
+  }
 }
 
 export default SpecializationItem;
--- portfolio-app/src/components/Specializations/SpecializationItems/SpecializationItems.js
+++ portfolio-app/src/components/Specializations/SpecializationItems/SpecializationItems.js
@@ -3,11 +3,9 @@ import SpecializationItem from './SpecializationItem/SpecializationItem';
 import './index.css';
 
 export class SpecializationItems extends Component {
-    render() {
-        return this.props.items.map((item) => (
-            <SpecializationItem item={item} />
-        ));
-    }
+  render() {
+    return this.props.items.map((item) => <SpecializationItem item={item} />);
+  }
 }
 
 export default SpecializationItems;
