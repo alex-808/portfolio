1af24da2c9f467c382711bbce1c1629516e2bca2
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-20 14:45:27 -0700

    Add yellow accent shader for sims

--- portfolio-app/src/components/Animation/Animation.js
+++ portfolio-app/src/components/Animation/Animation.js
@@ -49,6 +49,26 @@ export class Animation extends Component {
       gl_FragColor = vec4(c, 1.0);
     }
   `;
+
+    var accentFragmentShader2 = `
+    
+    varying vec3 vPos;
+    uniform vec3 size;
+    uniform float thickness;
+    uniform float smoothness;
+   
+    void main() {
+            
+      float a = smoothstep(thickness, thickness + smoothness, length(abs(vPos.xy) - size.xy));
+      a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.yz) - size.yz));
+      a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.xz) - size.xz));
+      
+      vec3 c = mix(vec3(0), vec3(1.0000, 0.8549, 0.0003), a);
+      
+      gl_FragColor = vec4(c, 1.0);
+    }
+  `;
+
     let renderer, stats;
     let physics;
 
@@ -177,7 +197,7 @@ export class Animation extends Component {
             },
           },
           vertexShader: vertexShader,
-          fragmentShader: accentFragmentShader,
+          fragmentShader: accentFragmentShader2,
         });
         sceneInfo.geometry = geometryBox;
         sceneInfo.material = material;
