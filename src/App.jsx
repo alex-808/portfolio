091d7defa886b3e93c0909fbbef7620f67cf8107
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-28 16:52:35 -0700

    Add Netlify config and build folder

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    fetch('/functions/email.js')
+    fetch('.netlify/functions/email')
       .then((res) => res.json())
       .then((json) => console.log(json));
     this.state = {
