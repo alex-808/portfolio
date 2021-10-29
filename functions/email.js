5cc65d14305626be9f75771c4ff70e2f732b42f1
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-29 12:15:47 -0700

    Add SendGrid email request

--- functions/email.js
+++ functions/email.js
@@ -1,5 +1,23 @@
 exports.handler = async function (event, context) {
-  console.log('hello world');
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: process.env.EMAIL_ADDRESS, // Change to your recipient
+    from: process.env.EMAIL_ADDRESS, // Change to your verified sender
+    subject: 'Hello from the other side',
+    text: "It's working",
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log('worked');
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello World' }),
