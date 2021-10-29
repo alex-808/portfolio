8561ca0e15e5af585b716b15d45f3a8e8de4953a
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-29 12:58:51 -0700

    Serverless function debugging

--- functions/email.js
+++ functions/email.js
@@ -1,5 +1,7 @@
 exports.handler = async function (event, context) {
   const sgMail = require('@sendgrid/mail');
+  console.log('API key:', process.env.SENDGRID_API_KEY);
+  console.log('Email address:', process.env.EMAIL_ADDRESS);
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
@@ -9,17 +11,22 @@ exports.handler = async function (event, context) {
     text: "It's working",
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
+  console.log('worked');
   sgMail
     .send(msg)
     .then(() => {
       console.log('Email sent');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Email Sent' }),
+      };
     })
     .catch((error) => {
       console.error(error);
+      console.log('Email not sent');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Email Not Sent' }),
+      };
     });
-  console.log('worked');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Email Sent' }),
-  };
 };
