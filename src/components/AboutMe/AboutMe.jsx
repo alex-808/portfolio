e858f912be1181eee2d0cc1dc0a31d089af24c29
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-18 15:53:57 -0800

    Refactor AboutMe to functional component

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
 import { ContactForm } from '../ContactForm/ContactForm';
 import './index.css';
 
-export class AboutMe extends Component {
-  render() {
-    return (
-      <section className="about_me_section">
-        <h1 className="about_me_title">{this.props.about_me.title}</h1>
-        <div className="about_me_text">{this.props.about_me.text}</div>
-        <ContactForm />
-      </section>
-    );
-  }
-}
+const AboutMe = ({ about_me = { title: '', text: '' } }) => {
+  return (
+    <section className="about_me_section">
+      <h1 className="about_me_title">{about_me.title}</h1>
+      <div className="about_me_text">{about_me.text}</div>
+      <ContactForm />
+    </section>
+  );
+};
 
 export default AboutMe;
