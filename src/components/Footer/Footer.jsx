63a0bd2d187184c46b1a7e8e602a0210890b122e
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-18 13:52:49 -0800

    Refactor Footer as functional component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
 import './index.css';
 import SocialIcons from './SocialIcons/SocialIcons';
 
-export class Footer extends Component {
-  render() {
-    return (
-      <section className="footer_section">
-        <div className="footer_accent">{this.props.footer.cta}</div>
-        <div className="social_icons">
-          <SocialIcons social_icons={this.props.footer.social_icons} />
-        </div>
-        <div className="footer_animation"></div>
-      </section>
-    );
-  }
-}
+const Footer = ({ footer = { cta: '', social_icons: [] } }) => {
+  return (
+    <section className="footer_section">
+      <div className="footer_accent">{footer.cta}</div>
+      <div className="social_icons">
+        <SocialIcons social_icons={footer.social_icons} />
+      </div>
+      <div className="footer_animation"></div>
+    </section>
+  );
+};
 
 export default Footer;
