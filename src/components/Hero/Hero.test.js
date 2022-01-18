caf7c7075beccf4cf2978c2566a73c8b1a9fe870
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-18 11:59:33 -0800

    Add/Update Hero tests

--- src/components/Hero/Hero.test.js
+++ src/components/Hero/Hero.test.js
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { getQueriesForElement } from '@testing-library/react';
 
 import Hero from './Hero';
 
-test('hero.title prop sets Hero title', () => {
-  const root = document.createElement('div');
-  ReactDOM.render(<Hero hero={{ title: 'HeroTitle' }} />, root);
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
 
-  expect(root.querySelector('h1').textContent).toBe('HeroTitle');
+afterEach(() => {
+  // cleanup on exiting
+  ReactDOM.unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// Testing component from user perspective
+test('Title prop text is present in Hero component', () => {
+  ReactDOM.render(<Hero hero={{ title: 'Hello' }} />, container);
+
+  const { getByText } = getQueriesForElement(container);
+  getByText('Hello');
+});
+
+test('Text prop text is present in Hero component', () => {
+  ReactDOM.render(<Hero hero={{ text: 'This is test text' }} />, container);
+
+  const { getByText } = getQueriesForElement(container);
+  getByText('This is test text');
 });
