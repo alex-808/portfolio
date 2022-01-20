b97eb89ad6420acdd4fb9fd61dabcfeeef64d3f9
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-19 19:49:08 -0800

    Refactor <Specializations/> to functional component

--- src/components/Specializations/Specializations.jsx
+++ src/components/Specializations/Specializations.jsx
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import SpecializationItems from './SpecializationItems/SpecializationItems';
 import './index.css';
 
-export class Specializations extends Component {
-    render() {
-        return (
-            <section className="spec_section">
-                <div className="spec_header_box"></div>
-                <div className="spec_item_container">
-                    <SpecializationItems items={this.props.spec.items} />
-                </div>
-            </section>
-        );
-    }
-}
+const Specializations = ({ items = [] }) => {
+  return (
+    <section className="spec_section">
+      <div className="spec_header_box"></div>
+      <div className="spec_item_container">
+        <SpecializationItems items={items} />
+      </div>
+    </section>
+  );
+};
 
 export default Specializations;
