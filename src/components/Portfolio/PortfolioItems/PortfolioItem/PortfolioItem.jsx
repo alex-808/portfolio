647d93df7ce29c124b29b4d4f4880d358f3707cd
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-26 16:10:12 -0800

    Refactor PortfolioItem comp to functional comp

--- src/components/Portfolio/PortfolioItems/PortfolioItem/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItems/PortfolioItem/PortfolioItem.jsx
@@ -1,47 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
-export class PortfolioItem extends Component {
-  constructor(props) {
-    super(props);
-    Aos.init({ offset: 0, duration: 1500, easing: 'ease-in-out' });
+const PortfolioItem = ({
+  project = {
+    title: '',
+    side_class: '',
+    accentColor: '',
+    img: '',
+    text: '',
+  },
+}) => {
+  project.links = project.links || [];
 
-    if (props.project.side_class.includes('port_right')) {
-      this.slideDir = 'left';
-    } else {
-      this.slideDir = 'right';
-    }
-  }
-  render() {
-    return (
-      <div data-aos={`slide-${this.slideDir}`} className="proj_container">
-        <div className="proj_box_img"></div>
-        <div className={this.props.project.side_class}>
-          <div className="proj_title_container">
-            <h3 className="proj_title">{this.props.project.title}</h3>
-            <div className="port_accent_container">
-              <div
-                className={`port_accent-${this.props.project.accentColor}`}
-              ></div>
-            </div>
+  Aos.init({ offset: 0, duration: 1500, easing: 'ease-in-out' });
+
+  const slideDir = project.side_class?.includes('port_right')
+    ? 'left'
+    : 'right';
+  return (
+    <div data-aos={`slide-${slideDir}`} className="proj_container">
+      <div className="proj_box_img"></div>
+      <div className={project.side_class}>
+        <div className="proj_title_container">
+          <h3 className="proj_title">{project.title}</h3>
+          <div className="port_accent_container">
+            <div className={`port_accent-${project.accentColor}`}></div>
           </div>
-          <div className="proj_img_container">
-            <img className="proj_img" src={this.props.project.img} />
-            <div className="proj_img_tab"></div>
-          </div>
-          <div className="proj_text">{this.props.project.text}</div>
-          {this.props.project.links.map((link, i) => (
-            <a href={link.url} key={i}>
-              {link.text}
-            </a>
-          ))}
         </div>
-        <div className="port_divider"></div>
-        <div className={`proj_animation_${this.slideDir}`}></div>
+        <div className="proj_img_container">
+          <img
+            className="proj_img"
+            src={project.img}
+            alt="Project on computer screen"
+          />
+          <div className="proj_img_tab"></div>
+        </div>
+        <div className="proj_text">{project.text}</div>
+        {project.links.map((link, i) => (
+          <a href={link.url} key={i}>
+            {link.text}
+          </a>
+        ))}
       </div>
-    );
-  }
-}
+      <div className="port_divider"></div>
+      <div className={`proj_animation_${slideDir}`}></div>
+    </div>
+  );
+};
 
 export default PortfolioItem;
