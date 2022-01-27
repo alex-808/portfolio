88850601cbe4b814f99f9f2a58855629b2dccdb1
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-26 16:10:59 -0800

    Refactor Portfolio component to functional component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './index.css';
 import PortfolioItems from './PortfolioItems/PortfolioItems';
 
-export class Portfolio extends Component {
-  render() {
-    return (
-      <section className="port_section">
-        <h1 className="port_title">{this.props.port.port_title}</h1>
-        <div className="port_title_underline"></div>
-        <div className="port_container">
-          <PortfolioItems projects={this.props.port.projects} />
-        </div>
-      </section>
-    );
-  }
-}
+const Portfolio = ({ port = { projects: [], port_title: '' } }) => {
+  port.projects = port.projects || [];
+  return (
+    <section className="port_section">
+      <h1 className="port_title">{port.port_title}</h1>
+      <div className="port_title_underline"></div>
+      <div className="port_container">
+        <PortfolioItems projects={port.projects} />
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
